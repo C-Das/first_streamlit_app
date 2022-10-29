af345c9c49825a3639ce7f46dcdc064484f5bd9b
Chinmay Das <chinmay98@gmail.com>
2022-10-29 13:03:10 -0400

    Update streamlit_app.py
    updated the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
 import streamlit 
 streamlit.title ("My parents healthy diner") 
 
-streamlit.header("Breakfast Menu")
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
-
-
