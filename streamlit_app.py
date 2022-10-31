097a05fcb131f5a1a55af5a84afa8398abec7fab
Chinmay Das <chinmay98@gmail.com>
2022-10-31 06:00:57 -0400

    Update streamlit_app.py
    typo fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit 
 import pandas
 import requests
-import snowflake-connector
+import snowflake.connector
 streamlit.title ("My parents healthy diner") 
 
 streamlit.header('Breakfast Menu')
